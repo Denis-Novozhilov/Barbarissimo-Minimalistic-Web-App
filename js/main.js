547c49c50032fc2736bb75364006f035de1ccb3d
Denis-Matyukha <denmatuhawork@gmail.com>
2020-10-26 04:38:32 +0300

    mode selector is added

--- js/main.js
+++ js/main.js
@@ -5,18 +5,13 @@ const answerString = document.querySelector(`#answer-string`);
 const chechButton = document.querySelector(`#check-button`);
 
 const askNewQuestion = function() {
-    // taskString.innerText = `${window.vocabulary.TasksAndAnswers[0].question}`;
-    // let str = window.vocabulary.TasksAndAnswers[0].question;
-    // return str;
-    // taskString.innerText = str.toArray();
+
 };
 askNewQuestion();
 
 let str = `${window.vocabulary.Tasks[0].Spanish}`;
 console.log(Array.from(str.toLowerCase().split(' ')));
-
 /*
-
 console.log(window.vocabulary.Tasks[0][window.mode.EnglishByRussian.Question]);
 VM317:1 It is entertaining because students learn robotics and also learn computer programming.
 
