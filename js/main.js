ba2568a7c45b2b6e00b54f6c7084ae9add2af608
Denis-Matyukha <denmatuhawork@gmail.com>
2020-10-26 05:06:44 +0300

    css syle is corrected

--- js/main.js
+++ js/main.js
@@ -3,14 +3,20 @@
 const taskString = document.querySelector(`#task-string`);
 const answerString = document.querySelector(`#answer-string`);
 const chechButton = document.querySelector(`#check-button`);
+const modeSelector = document.querySelector(`#mode`);
 
 const askNewQuestion = function() {
+    let selectedMode = modeSelector.value;
+    let questionLang = window.mode[selectedMode].Question;
+    let answerLang = window.mode[selectedMode].Answer;
 
+    taskString.innerText = `${window.vocabulary.Tasks[0][questionLang]}`;
+    
 };
 askNewQuestion();
 
-let str = `${window.vocabulary.Tasks[0].Spanish}`;
-console.log(Array.from(str.toLowerCase().split(' ')));
+// let str = `${window.vocabulary.Tasks[0].Spanish}`;
+// console.log(Array.from(str.toLowerCase().split(' ')));
 /*
 console.log(window.vocabulary.Tasks[0][window.mode.EnglishByRussian.Question]);
 VM317:1 It is entertaining because students learn robotics and also learn computer programming.
