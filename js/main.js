56e58a2c546b2f6b7d65d208eee75a494d668257
Denis-Matyukha <denmatuhawork@gmail.com>
2020-10-26 03:39:06 +0300

    vocabulary is modifyed and mode js file is created

--- js/main.js
+++ js/main.js
@@ -12,5 +12,12 @@ const askNewQuestion = function() {
 };
 askNewQuestion();
 
-let str = `${window.vocabulary.TasksAndAnswers[0].question}`;
+let str = `${window.vocabulary.Tasks[0].Spanish}`;
 console.log(Array.from(str.toLowerCase().split(' ')));
+
+/*
+
+console.log(window.vocabulary.Tasks[0][window.mode.EnglishByRussian.Question]);
+VM317:1 It is entertaining because students learn robotics and also learn computer programming.
+
+ */
---
+++ js/mode.js
@@ -0,0 +1,36 @@
+"use strict";
+
+(function () {
+
+    window.mode = {
+
+        RussianByEnglish: {
+            Question: `Russian`,
+            Answer: `English`,
+        },
+        RussianBySpanish: {
+            Question: `Russian`,
+            Answer: `Spanish`,
+        },
+
+        EnglishByRussian: {
+            Question: `English`,
+            Answer: `Russian`,
+        },
+        EnglishBySpanish: {
+            Question: `English`,
+            Answer: `Spanish`,
+        },
+
+        SpanishByRussian: {
+            Question: `Spanish`,
+            Answer: `Russian`,
+        },
+        SpanishByEnglish: {
+            Question: `Spanish`,
+            Answer: `English`,
+        },
+
+    };
+
+})();
