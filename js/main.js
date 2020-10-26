982c6640d443d0c9f357ddc2cd2d7290d98107cd
Denis-Matyukha <denmatuhawork@gmail.com>
2020-10-26 14:50:34 +0300

    stable condition with verification

--- js/main.js
+++ js/main.js
@@ -7,20 +7,27 @@ const modeSelector = document.querySelector(`#mode`);
 const propArea = document.querySelector(`.proposeds`);
 
 
-let randomTaskNumber = window.randomiser.getRandomFromInterval(0, 9);
+
+
 
 const askNewQuestion = function() {
 
+    answerArea.innerHTML = ``;
+    propArea.innerHTML = ``;
+    
+    let randomTaskNumber = window.randomiser.getRandomFromInterval(0, 9);
     let selectedMode = modeSelector.value;
     let questionLang = window.mode[selectedMode].Question;
     let answerLang = window.mode[selectedMode].Answer;
-    let questionString = window.vocabulary.Tasks[randomTaskNumber][questionLang];
-    let answerString = window.vocabulary.Tasks[randomTaskNumber][answerLang];
-    
-    taskArea.innerText = `${questionString}`;
-    
 
-    
+    let currentTask = window.vocabulary.Tasks[randomTaskNumber];
+
+    window.currentTaskItem = currentTask;
+
+    let questionString = currentTask[questionLang];
+    let answerString = currentTask[answerLang];
+
+    taskArea.innerText = `${questionString}`;
     
     let arrQuestion = window.randomiser.getArrFromString(questionString);
     let arrAnswer = window.randomiser.getArrFromString(answerString);
@@ -60,34 +67,82 @@ modeSelector.addEventListener('change', function(){
     askNewQuestion();
 });
 
-checkButton.addEventListener('click', function(evt){
-    // console.log(evt.target);
-    let selectedMode = modeSelector.value;
 
+checkButton.addEventListener('click', function(evt){
+    let currentTask = window.currentTaskItem;
+
+    let selectedMode = modeSelector.value;
+    let answerLang = window.mode[selectedMode].Answer;
+    let rightAnswer = (currentTask[answerLang].toLowerCase());
+
+    // console.log(typeof rightAnswer);
+    // let preClean = rightAnswer.toLowerCase();
+    console.log(rightAnswer.length);
+    let cleanRightAnswer = rightAnswer.slice(0, rightAnswer.length-1);
+    cleanRightAnswer = cleanRightAnswer.replace(/, /g, ` `);
+
+    console.log(`HERE->[${cleanRightAnswer}]`);
+
+    // console.log(`!!![${cleanRightAnswer}]!!!`);
+
+
+    let usersAnswer = answerArea.textContent;
+
+    // console.log(usersAnswer);
+    // console.log(typeof usersAnswer);
+
+    let cleanAnswer = (usersAnswer.replace(/  /g, ` `).slice(1));
+    let cleanUserAnswer = cleanAnswer.slice(0, cleanAnswer.length-1);
+    console.log(`HERE->[${cleanUserAnswer}]`);
+
+    console.log(cleanRightAnswer === cleanUserAnswer);
+
+    if (cleanRightAnswer === cleanUserAnswer) {
+        taskArea.classList.add(`victory`);
+        answerArea.classList.add(`victory`);
+        propArea.classList.add(`victory`);
+        propArea.innerHTML = `Правильно =^_^=`;
+        setTimeout(function(){
+            taskArea.classList.remove(`victory`);
+            answerArea.classList.remove(`victory`);
+            propArea.classList.remove(`victory`);
+            propArea.innerHTML = ``;
+            askNewQuestion();
+        },2600);
+    } else {
+        taskArea.classList.add(`fail`);
+        answerArea.classList.add(`fail`);
+        setTimeout(function(){
+            taskArea.classList.remove(`fail`);
+            answerArea.classList.remove(`fail`);
+        },1900);
+    }
+});
+
+
+/*
+    let selectedMode = modeSelector.value;
     let questionLang = window.mode[selectedMode].Question;
     let answerLang = window.mode[selectedMode].Answer;
 
-    // let questionString = window.vocabulary.Tasks[randomTaskNumber][questionLang];
+    let questionString = window.vocabulary.Tasks[randomTaskNumber][questionLang];
 
-    let writeAnswer = window.vocabulary.Tasks[randomTaskNumber][answerLang].toLowerCase();
+    let writeAnswer = window.vocabulary.Tasks[randomTaskNumber][answerLang];
+
+    writeAnswer = writeAnswer.toLowerCase();
+
+    writeAnswer = Array.from(writeAnswer);
     // writeAnswer = Array.from(writeAnswer);
-    console.log(writeAnswer.typeof);
+    // console.log(x);
+    writeAnswer = writeAnswer.filter(function(el) {
+        return el != null;
+    });
+
+    console.log(writeAnswer[20]);
+
     // writeAnswer = Object.toString(writeAnswer);
 
     let usersAnswer = answerArea.textContent;
-    console.log(usersAnswer.typeof);
-    // usersAnswer = Array.from(answerArea.textContent);
-    // let usersAnswer2 = usersAnswer.split(`,`);
-    // console.log(`2!!! ${usersAnswer2}`);
 
-    // usersAnswer = userAnswer.filter(function(element) {
-    //         return element != null;
-    //     });
-
-    // console.log(writeAnswer);
-    // console.log(writeAnswer.lenght);
-    // console.log(writeAnswer.typeof);
-    // console.log(userAnswer);
-    // console.log(userAnswer.lenght);
-    // console.log(userAnswer.typeof);
-});
+    console.log(usersAnswer);
+ */
